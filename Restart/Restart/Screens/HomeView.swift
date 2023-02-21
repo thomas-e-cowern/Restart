37ec0ad32dee513297abaae2834e7f06535524fb
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-21 12:45:27 -0500

    add ZStack and conditional to ContentView

---
+++ Restart/Restart/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Thomas Cowern on 2/21/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Thomas Cowern on 2/21/23.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
