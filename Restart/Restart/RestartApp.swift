f1dff8049fc11778397137824ddd337e88740ba6
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-21 11:40:07 -0500

    update .gitignore

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Thomas Cowern on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
