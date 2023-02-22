1cceabbcbe06d413719460c246b7cd6ba4aea15a
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-22 10:28:00 -0500

    updae Home screen with Image, Text and custom Button independently

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -13,16 +13,53 @@ struct HomeView: View {
     
     // MARK: - Body
     var body: some View {
-        VStack {
-            Text("Home")
-                .font(.largeTitle)
-            
-            Button {
-                isOnboardingViewActive = true
-            } label: {
-                Text("Restart")
-            }
+        VStack(alignment: .center) {
+            Color(.white)
+                .ignoresSafeArea(.all)
 
+            // MARK: Header
+            ZStack {
+                Circle()
+                    .stroke(.gray.opacity(0.2), lineWidth: 40)
+                    .frame(width: 260, height: 260, alignment: .center)
+                Circle()
+                    .stroke(.gray.opacity(0.2), lineWidth: 80)
+                    .frame(width: 260, height: 260, alignment: .center)
+                
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+            }
+            //: End of ZStack
+            
+            // MARK: Center
+            Text("The time that leads to mastery is dependent on the intensity of our focus")
+                .multilineTextAlignment(.center)
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.black)
+                .padding(.horizontal, 20)
+            
+            
+            // MARK: Footer
+            ZStack {
+                Capsule()
+                    .fill(Color("ColorBlue"))
+                    .frame(width: 160, height: 60)
+                
+                HStack {
+                    Image(systemName: "target")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24, weight: .bold))
+                    
+                    Text("Restart")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                }
+                
+            } //: End of ZStack
+            
+            Spacer()
         }  //: End of VStack
     }
 }
