ac4c87a63533cb256de9e8470c6177d06ae7cbac
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-22 09:59:27 -0500

    add comples button to OnboardingView

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -51,6 +51,51 @@ struct OnboardingView: View {
                 
                 Spacer()
                 // MARK: Footer
+                ZStack {
+                    // MARK: Static background
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(.white.opacity(0.2))
+                        .padding(8)
+                     
+                    // MARK: Call to action
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // MARK: Capsule dynamic
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    // MARK: Circle draggable
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        } //: End of ZStack
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        
+                        Spacer()
+                    } //: End of HStack
+                    
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
             } //: End of VStack
         } //: End of ZStack
     }
