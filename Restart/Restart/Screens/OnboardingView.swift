cb92bd8ce1ecfdc4c1ac57cb4c83aeb37f5d3900
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-02-23 14:53:49 -0500

    update titleText to change during animation

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -16,6 +16,7 @@ struct OnboardingView: View {
     @State private var isAnimating: Bool = false
     @State private var imageOffset: CGSize = .zero
     @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share"
     
     // MARK: Body
     var body: some View {
@@ -28,10 +29,11 @@ struct OnboardingView: View {
                 Spacer()
                 
                 VStack {
-                    Text("Share")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .transition(.opacity)
                     
                     Text("It's not how much we give, but how much love we put into giving")
                         .multilineTextAlignment(.center)
@@ -65,6 +67,7 @@ struct OnboardingView: View {
                                 
                                 withAnimation(.linear(duration: 0.25)) {
                                     indicatorOpacity = 0
+                                    textTitle = "Give"
                                 }
                             }
                         })
@@ -73,6 +76,7 @@ struct OnboardingView: View {
                                 
                                 withAnimation(.linear(duration: 0.25)) {
                                     indicatorOpacity = 1
+                                    textTitle = "Share"
                                 }
                             }))
                         .animation(.easeIn(duration: 1), value: imageOffset)
